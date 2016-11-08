2cbd86fac3154c4127b8c2808b9907031bcaff1f
Beefstyles <glenwillis@gmail.com>
2016-11-08 20:04:44 +0800

    Added some more inputs there.

--- ProcessSimLevelEditor/ProcessSimLevelEditor/MainWindow.xaml.cs
+++ ProcessSimLevelEditor/ProcessSimLevelEditor/MainWindow.xaml.cs
@@ -223,6 +223,29 @@ namespace ProcessSimLevelEditor
         }
         private void SetJsonDetails(LevelOutputJSON levelOutput)
         {
+            /*
+            public string Title;
+            public int GridXSize;
+            public int GridYSize;
+            public int Capex;
+            public int LevelInletTemp;
+            public int LevelInletPress;
+            public string Objective1Text;
+            public string Objective2Text;
+            public string Objective3Text;
+            public float Objective1Value;
+            public float Objective2Value;
+            public float Objective3Value;
+            public int CostPlatMax;
+            public int CostGoldMax;
+            public int CostSilverMax;
+            public int CostBronzeMax;
+            public int CostPassMax;
+            public char[] GridArray;
+            public int Methane, Ethane, Propane, NButane, IButane, NPentane,
+            IPentane, Hexane, Benzene, Heptane, Octane, Nonane, Decane,
+            Water, Nitrogen, CO2, H2S;
+            */
             levelOutput.Title = "TestTitle";
         }
 
