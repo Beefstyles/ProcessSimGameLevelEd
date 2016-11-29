d3a1e95d45418d065d1dca18667a4c843a92c7e8
Beefstyles <glenwillis@gmail.com>
2016-11-29 20:56:17 +0800

    Added more phase returning.

--- ProcessSimLevelEditor/ProcessSimLevelEditor/PhaseCalc.cs
+++ ProcessSimLevelEditor/ProcessSimLevelEditor/PhaseCalc.cs
@@ -22,6 +22,54 @@ namespace ProcessSimLevelEditor
                 case "Propane":
                     returnPhase = setPressure <= (9835.1 * Math.Log(setTemp) - 48464) ? 1 : 0;
                     break;
+                case "nButane":
+                    returnPhase = setPressure <= (10196 * Math.Log(setTemp) - 52175) ? 1 : 0;
+                    break;
+                case "iButane":
+                    returnPhase = setPressure <= (10196 * Math.Log(setTemp) - 52175) ? 1 : 0;
+                    break;
+                case "nPentane":
+                    returnPhase = setPressure <= (0.0003 * Math.Pow(setTemp, 3) - 0.2002 * Math.Pow(setTemp, 2) + 48.614 * setTemp - 3916.6) ? 1 : 0;
+                    break;
+                case "iPentane":
+                    returnPhase = setPressure <= (0.0003 * Math.Pow(setTemp, 3) - 0.2002 * Math.Pow(setTemp, 2) + 48.614 * setTemp - 3916.6) ? 1 : 0;
+                    break;
+                case "Benzene":
+                    returnPhase = setPressure <= (0.0003 * Math.Pow(setTemp, 3) - 0.2265 * Math.Pow(setTemp, 2) + 67.137 * setTemp - 6612.4) ? 1 : 0;
+                    break;
+                case "Hexane":
+                    returnPhase = setPressure <= (0.0002 * Math.Pow(setTemp, 3) - 0.1758 * Math.Pow(setTemp, 2) + 47.795 * setTemp - 4310.9) ? 1 : 0;
+                    break;
+                case "Heptane":
+                    returnPhase = setPressure <= (-0.0161 * Math.Pow(setTemp, 2) + 16.404 * setTemp - 3203.8) ? 1 : 0;
+                    break;
+                case "Octane":
+                    //To be implemented
+                    returnPhase = setPressure <= (-0.0161 * Math.Pow(setTemp, 2) + 16.404 * setTemp - 3203.8) ? 1 : 0;
+                    break;
+                case "Nonane":
+                    //To be implemented
+                    returnPhase = setPressure <= (-0.0161 * Math.Pow(setTemp, 2) + 16.404 * setTemp - 3203.8) ? 1 : 0;
+                    break;
+                case "Decane":
+                    //To be implemented
+                    returnPhase = setPressure <= (-0.0161 * Math.Pow(setTemp, 2) + 16.404 * setTemp - 3203.8) ? 1 : 0;
+                    break;
+                case "Water":
+                    returnPhase = setPressure <= (-0.0002 * Math.Pow(setTemp, 2) + .0948 * setTemp - 7.5362) ? 1 : 0;
+                    break;
+                case "Nitrogen":
+                    //To be implemented
+                    returnPhase = setPressure <= (-0.0161 * Math.Pow(setTemp, 2) + 16.404 * setTemp - 3203.8) ? 1 : 0;
+                    break;
+                case "CO2":
+                    //To be implemented
+                    returnPhase = setPressure <= (-0.0161 * Math.Pow(setTemp, 2) + 16.404 * setTemp - 3203.8) ? 1 : 0;
+                    break;
+                case "H2S":
+                    //To be implemented
+                    returnPhase = setPressure <= (-0.0161 * Math.Pow(setTemp, 2) + 16.404 * setTemp - 3203.8) ? 1 : 0;
+                    break;
                 default:
                     returnPhase = 999; //Phase not implemented
                     break;
