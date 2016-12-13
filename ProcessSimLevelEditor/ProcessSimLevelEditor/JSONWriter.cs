cd44e270f430cdd56a62c9ae4f7998ecb26f9b1d
Beefstyles <glenwillis@gmail.com>
2016-12-13 21:30:08 +0800

    Saving level works now.

--- ProcessSimLevelEditor/ProcessSimLevelEditor/JSONWriter.cs
+++ ProcessSimLevelEditor/ProcessSimLevelEditor/JSONWriter.cs
@@ -24,21 +24,20 @@ namespace ProcessSimLevelEditor
             }
         }
 
-        private void WriteJsonFile(LevelOutputJSON levelOutput, string filename)
+        private void WriteJsonFile(LevelOutputJSON levelOutput, string savePath)
         {
             JsonSerializer serialiser = new JsonSerializer();
             serialiser.NullValueHandling = NullValueHandling.Ignore;
             string myDocsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            //using (StreamWriter sw = new StreamWriter(myDocsPath + @"\ProcessSimGameEd\jsonTest.txt"))
-            string savePath = (@"\ProcessSimGameEd\" + filename + ".txt");
-            using (StreamWriter sw = new StreamWriter(myDocsPath + savePath))
+
+            using (StreamWriter sw = new StreamWriter(savePath))
             using (JsonWriter writer = new JsonTextWriter(sw))
             {
                 serialiser.Formatting = Formatting.Indented;
                 serialiser.Serialize(writer, levelOutput);
             }
 
-            MessageBox.Show("Writing json done correctly, look in " + myDocsPath + "\\ProcessSimGameEd");
+            MessageBox.Show("Writing json done correctly, look in " + savePath);
         }
     }
 }
